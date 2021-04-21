2d6228228c796aa34355a13dd1bd854ec1547e48
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-04-21 18:38:05 -0300

    feat: adicionando o CreatePiggyBank

--- lib/domain/usecases/piggy_bank/piggy_bank_information.dart
+++ lib/domain/usecases/piggy_bank/piggy_bank_information.dart
@@ -3,3 +3,7 @@ import '../../entities/entities.dart';
 abstract class GetListPiggyBank {
   Future<List<PiggyBankEntity>> getListPiggyBank();
 }
+
+abstract class CreatePiggyBank {
+  Future<bool> createPiggyBank(String name, [String description]);
+}
